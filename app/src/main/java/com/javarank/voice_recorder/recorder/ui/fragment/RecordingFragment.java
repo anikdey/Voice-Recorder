4486cc596feb26aec46ea6d8549ac1ef8d0bdc33
Anik Dey <anik.dey@selise.com.bd>
2018-05-09 19:41:42 +0600

    Recording done and saved

--- app/src/main/java/com/javarank/voice_recorder/recorder/ui/fragment/RecordingFragment.java
+++ app/src/main/java/com/javarank/voice_recorder/recorder/ui/fragment/RecordingFragment.java
@@ -1,8 +1,11 @@
 package com.javarank.voice_recorder.recorder.ui.fragment;
 
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +13,21 @@ import android.view.ViewGroup;
 import com.javarank.voice_recorder.R;
 import com.javarank.voice_recorder.common.BaseSupportFragment;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import butterknife.OnClick;
+
 
 public class RecordingFragment extends BaseSupportFragment {
 
     public static final String TAG = RecordingFragment.class.getSimpleName();
+
+    private static final String FILE_TYPE = ".mp4";
+    private String fileName = null;
+    private MediaRecorder mediaRecorder;
 
     public static RecordingFragment getNewInstance() {
         RecordingFragment fragment = new RecordingFragment();
@@ -28,6 +42,47 @@ public class RecordingFragment extends BaseSupportFragment {
     @Override
     public void init() {
 
+    }
+
+    private String getGeneratedFilePath() {
+        String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() +"/VoiceRecorder/";
+        File file = createFile(filePath, System.currentTimeMillis() + FILE_TYPE);
+        fileName = file.getAbsolutePath();
+        return fileName;
+    }
+
+    private File createFile(String filePath, String fileName) {
+        try {
+            fileName = URLDecoder.decode(fileName, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        File directory = new File(filePath);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+        File file = new File(directory.getAbsolutePath(), fileName);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+    @OnClick(R.id.start_recording)
+    protected void startRecording() {
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mediaRecorder.setOutputFile(getGeneratedFilePath());
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        try {
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+        } catch (IOException e) {
+            Log.e(TAG, "prepare() failed");
+        }
     }
 
     @Override
