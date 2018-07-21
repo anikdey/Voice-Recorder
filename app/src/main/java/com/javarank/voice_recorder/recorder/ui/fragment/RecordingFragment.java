9bc2178934b13f7c770733cb1b27c11e76e9263d
Anik Dey <info.anikdey003@gmail.com>
2018-07-21 10:59:31 +0600

    design update

--- app/src/main/java/com/javarank/voice_recorder/recorder/ui/fragment/RecordingFragment.java
+++ app/src/main/java/com/javarank/voice_recorder/recorder/ui/fragment/RecordingFragment.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.javarank.voice_recorder.R;
 import com.javarank.voice_recorder.common.BaseSupportFragment;
@@ -19,12 +21,27 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 
 
 public class RecordingFragment extends BaseSupportFragment {
 
     public static final String TAG = RecordingFragment.class.getSimpleName();
+
+
+    @BindView(R.id.start_recording_image_button)
+    ImageView startRecordingImageButton;
+    @BindView(R.id.delete_audio_image_button)
+    ImageView deleteAudioImageButton;
+    @BindView(R.id.pause_recording_image_button)
+    ImageView pauseRecordingImageButton;
+    @BindView(R.id.save_audio_image_button)
+    ImageView saveAudioImageButton;
+    @BindView(R.id.control_panel_linear_layout)
+    LinearLayout controlPanelLinearLayout;
+
+
 
     private static final String FILE_TYPE = ".mp4";
     private String fileName = null;
@@ -66,7 +83,10 @@ public class RecordingFragment extends BaseSupportFragment {
 
     //@OnClick(R.id.start_recording)
     protected void startRecording() {
-        mediaRecorder = new MediaRecorder();
+
+        controlPanelVisibility(View.VISIBLE);
+
+       /* mediaRecorder = new MediaRecorder();
         mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         mediaRecorder.setOutputFile(getGeneratedFilePath());
@@ -76,7 +96,11 @@ public class RecordingFragment extends BaseSupportFragment {
             mediaRecorder.start();
         } catch (IOException e) {
             Log.e(TAG, "prepare() failed");
-        }
+        }*/
+    }
+
+    private void controlPanelVisibility(int visibility) {
+        controlPanelLinearLayout.setVisibility(visibility);
     }
 
     private String getGeneratedFilePath() {
